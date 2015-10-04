876dae372ab80a93ebca1f45bc0854ac21dfec79
matt <matthew.l.lee@vanderbilt.edu>
2015-10-04 05:33:54 -0500

    header comments

--- SimpleTable/SimpleTableViewController.m
+++ SimpleTable/SimpleTableViewController.m
@@ -2,13 +2,9 @@
 //  Table
 //
 //  Author: Matt Lee
-//  github.com/mllee (though i dont even have this on github yet lol)
+//  VandyHacks fall 15
+//  github.com/mllee
 //
-//
-//  Shell from which I started by Simon on 1/12/13, (c) 2013 Appcoda
-//  He made a static view that just displayed a hard coded list;
-//  I started building from there so I wouldn't have to worry about the initial
-//  hooking up of the view controller.
 
 #import "SimpleTableViewController.h"
 
